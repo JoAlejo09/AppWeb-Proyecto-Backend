ceb14e8fa4bcc57067e9901ca02be66c48cb3f4f
Estudiante de Desarrollo de Software <71363459+JoAlejo09@users.noreply.github.com>
2025-08-02 13:37:40 -0500

    UPDATE CLOUDINARY-FILEUPLOAD

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import cloudinary from 'cloudinary'
+import fileUpload from "express-fileupload"
+
 import routerAdministrador from './routers/administrador_routes.js'
 import routerUsuario from './routers/usuario_routes.js'
 import routerPaciente from './routers/paciente_routes.js'
@@ -8,8 +11,19 @@ import routerAutenticacion from './routers/autenticacion_routes.js'
 
 
 const app = express()
+
 dotenv.config()
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+app.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : './uploads'
+}))
 app.set('port',process.env.PORT || 3000)
 app.use(cors())
 
