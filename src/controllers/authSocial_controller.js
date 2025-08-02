266e8e308b53fa716fd0264d76a6288d61756d0f
Estudiante de Desarrollo de Software <71363459+JoAlejo09@users.noreply.github.com>
2025-08-02 07:25:24 -0500

    UPDATE PASSPORT

---
+++ src/controllers/authSocial_controller.js
@@ -0,0 +1,22 @@
+import {crearTokenJWT} from '../middlewares/JWT.js'
+
+const authSocialCallback = async (req, res)=>{
+    try {
+        if(!req.usuario) return res.status(400).json({msg:"No se encontro el usuario"});
+        const token = crearTokenJWT(req.usuario._id, req.usuario.rol);
+        res.json({
+            token,
+            usuario:{
+                id: req.usuario._id,
+                nombre: req.usuario.nombre,
+                correo: req.usuario,correo,
+                rol: req.usuario.rol
+            }
+        });
+    } catch (error) {
+        console.error('Error en authSocialCallback:', error);
+    res.status(500).json({ msg: 'Error al autenticar con red social' });
+    }
+export{
+    authSocialCallback
+}
--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import cors from 'cors'
 import routerAdministrador from './routers/administrador_routes.js'
 import routerUsuario from './routers/usuario_routes.js'
 import routerPaciente from './routers/paciente_routes.js'
+import routerAutenticacion from './routers/autenticacion_routes.js'
+
 
 const app = express()
 dotenv.config()
@@ -20,6 +22,8 @@ app.get('/',(req,res)=>{
 app.use('/admin',routerAdministrador)
 app.use('/usuarios',routerUsuario)
 app.use ('/pacientes', routerPaciente)
+app.use('/auth', routerAutenticacion)
+
 
 app.use((req,res)=>res.status(404).send("Endpoint no encontrado - 404"))
 
