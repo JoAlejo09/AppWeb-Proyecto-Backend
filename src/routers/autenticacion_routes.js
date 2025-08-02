fe5789e9c3291c0b6ab87543bed9b4d68f1e79b7
Estudiante de Desarrollo de Software <71363459+JoAlejo09@users.noreply.github.com>
2025-08-02 08:14:43 -0500

    UPDATE

--- src/routers/autenticacion_routes.js
+++ src/routers/autenticacion_routes.js
@@ -8,7 +8,7 @@ router.get('/google', passport.authenticate('google', {scope:['profile', 'email'
 
 router.get ('/google/callback', passport.authenticate('google',{
     session: false,
-    failureRedirect: '/login'
+    failureRedirect: '/usuarios/login'
 }), (req, res) =>{
     const token = crearTokenJWT(req.user._id, req.user.rol)
     res.redirect(`${process.env.URL_FRONTEND}/oauth-success?token=${token}`)
@@ -17,7 +17,7 @@ router.get('/facebook', passport.authenticate('facebook', { scope: ['email'] }))
 
 router.get('/facebook/callback', passport.authenticate('facebook', {
   session: false,
-  failureRedirect: '/login'
+  failureRedirect: '/usuarios/login'
 }), (req, res) => {
   const token = crearTokenJWT(req.user._id, req.user.rol)
   res.redirect(`${process.env.URL_FRONTEND}/oauth-success?token=${token}`)
