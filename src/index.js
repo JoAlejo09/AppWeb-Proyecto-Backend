2470d0a13b031510913274a371438db57b084f78
Jose Pila <josealejandro_pilavizuete@hotmail.com>
2025-08-03 20:47:04 -0500

    update socket backend

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './config/passport.js'
 import http from 'http';
 import { Server } from 'socket.io';
 import Mensaje from './models/Mensaje.js';
+import jwt from 'jsonwebtoken';
 
 
 app.use(cors({ origin: "*" })); // en producción debes especificar el dominio
@@ -21,20 +22,42 @@ connection()
 const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
-        origin: ""
+        origin: "*",
+        methods: ["GET", "POST"],
     }
 })
 io.on('connection', (socket) => {
     console.log('Usuario conectado',socket.id)
-    socket.on('enviar-mensaje-front-back', async(payload)=>{
-        await Mensaje.create({
-            texto: payload.texto,
-            remitente: payload.remitente,
-            destinatario: payload.destinatarioId
-        });
+    socket.on('enviar-mensaje-front-back',(payload)=>{
         socket.broadcast.emit('enviar-mensaje-front-back',payload)
     })
 })
-server.listen(app.get('port') + 1, () => {
+io.listen((socket,next) => {
+    const token = socket.handshake.auth.token;
+    if (!token) {
+        return next(new Error('No autorizado'));
+    }
+    try {
+        const usuario = jwt.verify(token, process.env.JWT_SECRET);
+        socket.usuario = usuario;
+        next(); 
+    } catch (error) {
+        return next(new Error('Token inválido'));
+    }
+})
+io.use((socket, next) => {
+    const token = socket.handshake.auth.token;
+    if (!token) {
+        return next(new Error('No autorizado'));
+    }
+    try {
+        const usuario = jwt.verify(token, process.env.JWT_SECRET);
+        socket.usuario = usuario;
+        next();  
+    }   catch (error) {
+        return next(new Error('Token inválido'));
+    } 
+});  
+server.listen(app.get('port'), () => {
     console.log(`Socket.io server running on http://localhost:${app.get('port') + 1}`);
 });
